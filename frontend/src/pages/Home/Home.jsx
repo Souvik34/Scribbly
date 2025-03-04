ba2b8352d66f4fdeda80c91409f4e6beb875df92
Souvik <souvik13109@gmail.com>
2025-03-05 02:08:51 +0530

    Add button to open modal for adding new Note instances in Home page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,9 +1,17 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, {useState} from 'react'
 import Note from '../../components/Cards/Note'
+import { MdAdd } from 'react-icons/md'
+import { data } from 'react-router-dom'
 
 const Home = () => {
+  const [openAddEditModal, setOpenAddEditModal] = useState({
+    isShown: false,
+    data: null,
+    type:"add"
+  })
   return (
+    <>
     <div className='container mx-auto'>
       <div className=' grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 mt-8 max:md:m-5'>
         <Note
@@ -99,6 +107,11 @@ const Home = () => {
         
       </div>
     </div>
+
+    <button className='w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-900 absolute right-10 bottom-10' onClick={()=> 
+      setOpenAddEditModal({isShown: true, data: null, type: "add"})}> 
+      <MdAdd className='text-[32px] text-white'/></button>
+    </>
   )
 }
 
