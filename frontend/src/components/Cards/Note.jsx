83492e5d6cbce7098c4968ddf5035772510d6ede
Souvik <souvik13109@gmail.com>
2025-03-05 02:02:23 +0530

    Refactor Note component for improved readability and add multiple Note instances in Home page

--- frontend/src/components/Cards/Note.jsx
+++ frontend/src/components/Cards/Note.jsx
@@ -1,34 +1,34 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from 'react'
-import { MdOutlinePushPin} from 'react-icons/md'
+import { MdOutlinePushPin } from 'react-icons/md'
 import { MdCreate, MdDelete } from 'react-icons/md'
 
-const Note = ({title, date, content, isPinned, tags, onPinNote, onEdit, onDelete}) => {
+const Note = ({ title, date, content, isPinned, tags, onPinNote, onEdit, onDelete }) => {
   return (
-    <div className=' border rounded p-4 bg-white shadow-sm hover:shadow-xl transition-all ease-in-out'>
-    <div className='flex items-center justify-between'>
-        <div className=''>
-            <h6 className='text-sm font-medium'>
-                {title}
-                
-                </h6>
-            <span className='text-xs text-green-400'>
-                {date}
-             </span>
+    <div className='border rounded p-4 bg-white shadow-sm hover:shadow-xl transition-all ease-in-out'>
+      <div className='flex items-center justify-between'>
+        <div>
+          <h6 className='text-sm font-medium'>
+            {title}
+          </h6>
+          <span className='text-xs text-green-400'>
+            {date}
+          </span>
         </div>
 
-        <MdOutlinePushPin  className={`icon-btn ${isPinned ? "text-green-300" : "text-slate-400"}`
-      }
-    onClick={onPinNote}/>
-    </div>
-    <p className='text-xs text-slate-600 mt-2 '>
+        <MdOutlinePushPin
+          className={`icon-btn ${isPinned ? 'text-green-300' : 'text-slate-400'}`}
+          onClick={onPinNote}
+        />
+      </div>
+      <p className='text-xs text-slate-600 mt-2'>
         {content?.slice(0, 60)}
         </p>
 
 
         <div className='flex items-center justify-between mt-2'>
-            <div className='text-xs text-slate-500'>{tags}</div>
+            <div className='text-xs text-slate-500 bg-gray-200 rounded-sm'>{tags}</div>
 
             <div className='flex items-center gap-2'>
                 <MdCreate  className='icon-btn hover:text-green-500'
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -6,11 +6,97 @@ const Home = () => {
   return (
     <div className='container mx-auto'>
       <div className=' grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 mt-8 max:md:m-5'>
-        <Note/>
-        <Note/>
-        <Note/>
-        <Note/>
-        <Note/>
+        <Note
+        title={"Have Breakfast"}
+        date={"5th March, 2025"}
+        content={"I will eat a healthy breakfast today"}
+        tags={"#breakfast #healthy"}
+        isPinned={true}
+        onDelete={()=>{}}
+        onEdit={()=>{}}
+        onPinNote={()=>{}}
+        />
+        <Note
+        title={"Have Breakfast"}
+        date={"5th March, 2025"}
+        content={"I will eat a healthy breakfast today"}
+        tags={"#breakfast #healthy"}
+        isPinned={true}
+        onDelete={()=>{}}
+        onEdit={()=>{}}
+        onPinNote={()=>{}}
+        />
+        <Note
+        title={"Have Breakfast"}
+        date={"5th March, 2025"}
+        content={"I will eat a healthy breakfast today"}
+        tags={"#breakfast #healthy"}
+        isPinned={true}
+        onDelete={()=>{}}
+        onEdit={()=>{}}
+        onPinNote={()=>{}}
+        />
+        <Note
+        title={"Have Breakfast"}
+        date={"5th March, 2025"}
+        content={"I will eat a healthy breakfast today"}
+        tags={"#breakfast #healthy"}
+        isPinned={true}
+        onDelete={()=>{}}
+        onEdit={()=>{}}
+        onPinNote={()=>{}}
+        />
+        <Note
+        title={"Have Breakfast"}
+        date={"5th March, 2025"}
+        content={"I will eat a healthy breakfast today"}
+        tags={"#breakfast #healthy"}
+        isPinned={true}
+        onDelete={()=>{}}
+        onEdit={()=>{}}
+        onPinNote={()=>{}}
+        />
+        <Note
+        title={"Have Breakfast"}
+        date={"5th March, 2025"}
+        content={"I will eat a healthy breakfast today"}
+        tags={"#breakfast #healthy"}
+        isPinned={true}
+        onDelete={()=>{}}
+        onEdit={()=>{}}
+        onPinNote={()=>{}}
+        />
+        <Note
+        title={"Have Breakfast"}
+        date={"5th March, 2025"}
+        content={"I will eat a healthy breakfast today"}
+        tags={"#breakfast #healthy"}
+        isPinned={true}
+        onDelete={()=>{}}
+        onEdit={()=>{}}
+        onPinNote={()=>{}}
+        />
+        <Note
+        title={"Have Breakfast"}
+        date={"5th March, 2025"}
+        content={"I will eat a healthy breakfast today"}
+        tags={"#breakfast #healthy"}
+        isPinned={false}
+        onDelete={()=>{}}
+        onEdit={()=>{}}
+        onPinNote={()=>{}}
+        />
+        <Note
+        title={"Have Breakfast"}
+        date={"5th March, 2025"}
+        content={"I will eat a healthy breakfast today"}
+        tags={"#breakfast #healthy"}
+        isPinned={false}
+        onDelete={()=>{}}
+        onEdit={()=>{}}
+        onPinNote={()=>{}}
+        />
+        
       </div>
     </div>
   )
