02376e3f2682532189007c9a71a7daa200fce73b
Souvik <souvik13109@gmail.com>
2025-03-05 19:58:14 +0530

    Fix tag rendering in TagInput component to display individual tags correctly

--- frontend/src/components/Input/TagInput.jsx
+++ frontend/src/components/Input/TagInput.jsx
@@ -34,7 +34,7 @@ const addNewTag = () => {
         {tags?.length >0 &&(
             <div className='flex items-center gap-2 flex-wrap mt-2'>
                 {tags.map((tag, index) => (
-                    <span key={index} className='flex items-center gap-2 text-sm text-slate-900 bg-slate-100 px-3 py-1 rounded'> #{tags}
+                    <span key={index} className='flex items-center gap-2 text-sm text-slate-900 bg-slate-100 px-3 py-1 rounded'> #{tag}
 
                     <button onClick={()=> {
                         handleRemoveTag(tag)
